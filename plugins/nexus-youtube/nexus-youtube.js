cd72772558833dab22f54db3bb82a5b82bfc306b
bluebox-craig <craig.govender@bluebox.co.za>
2014-03-20 08:04:36 +0200

    update

--- plugins/nexus-youtube/nexus-youtube.js
+++ plugins/nexus-youtube/nexus-youtube.js
@@ -1,74 +1,74 @@
-document.addEventListener("DOMContentLoaded",function(){
-
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-},false);
-
-function onYouTubeIframeAPIReady() {
-	constructYouTubePlayers();
-}
-
-function constructYouTubePlayers(){
-
-	var players = document.getElementsByClassName("youtube-player");
-	for(var i=0; i<players.length; i++){
-		if(players[i].dataset.videoId){
-
-			players[i].id = players[i].id ? players[i].id : "youtube-player-"+players[i].dataset.videoId;
-
-			var player = new YT.Player(players[i].id,{
-				videoId:players[i].dataset.videoId,
-				playerVars:{
-					wmode:"transparent"
-				},
-				events:{
-					"onStateChange": onPlayerStateChange,
-					"onReady": onPlayerReady
+	document.addEventListener("DOMContentLoaded",function(){
+	
+		var tag = document.createElement('script');
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	
+	},false);
+	
+	function onYouTubeIframeAPIReady() {
+		constructYouTubePlayers();
+	}
+	
+	function constructYouTubePlayers(){
+	
+		var players = document.getElementsByClassName("youtube-player");
+		for(var i=0; i<players.length; i++){
+			if(players[i].dataset.videoId){
+				
+				players[i].dataset.videoId = players[i].dataset.videoId.replace("http://www.youtube.com/watch?v=","");
+				players[i].id = players[i].id ? players[i].id : "youtube-player-"+players[i].dataset.videoId;
+	
+				var player = new YT.Player(players[i].id,{
+					videoId:players[i].dataset.videoId,
+					playerVars:{
+						wmode:"transparent"
+					},
+					events:{
+						"onStateChange": onPlayerStateChange,
+						"onReady": onPlayerReady
+					}
+				});
+	
+				if(players[i].getAttribute("onStateChange")){
+					player.addEventListener("onStateChange",players[i].getAttribute("onStateChange"));
 				}
-			});
-
-			if(players[i].getAttribute("onStateChange")){
-				player.addEventListener("onStateChange",players[i].getAttribute("onStateChange"));
 			}
 		}
 	}
-}
-
-function onPlayerReady(event){
-	event.target.getIframe().dataset.state = "ready";
-}
-
-function onPlayerStateChange(event){
-
-	switch(event.target.getPlayerState()){
-		case -1:
-		event.target.getIframe().dataset.state = "unstarted";
-		break;
-
-		case 0:
-		event.target.getIframe().dataset.state = "ended";
-
-		case 1:
-		event.target.getIframe().dataset.state = "playing";
-		break;
-
-		case 2:
-		event.target.getIframe().dataset.state = "paused";
-		break;
-
-		case 3:
-		event.target.getIframe().dataset.state = "buffering";
-		break;
 	
-		case 5:
-		event.target.getIframe().dataset.state = "cued";
-		break;
-
-		default:
-		event.target.getIframe().dataset.state = "unknown";
-		break;		
+	function onPlayerReady(event){
+		event.target.getIframe().dataset.state = "ready";
 	}
-}
+	
+	function onPlayerStateChange(event){
+		switch(event.target.getPlayerState()){
+			case -1:
+			event.target.getIframe().dataset.state = "unstarted";
+			break;
+	
+			case 0:
+			event.target.getIframe().dataset.state = "ended";
+	
+			case 1:
+			event.target.getIframe().dataset.state = "playing";
+			break;
+	
+			case 2:
+			event.target.getIframe().dataset.state = "paused";
+			break;
+	
+			case 3:
+			event.target.getIframe().dataset.state = "buffering";
+			break;
+		
+			case 5:
+			event.target.getIframe().dataset.state = "cued";
+			break;
+	
+			default:
+			event.target.getIframe().dataset.state = "unknown";
+			break;		
+		}
+	}
