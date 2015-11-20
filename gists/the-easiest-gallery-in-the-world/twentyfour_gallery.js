39cea21d6bf9f2bcdafe910f8173725499b236c8
Craig Wayne <craig.wayne01@gmail.com>
2015-11-20 14:17:08 +0200

    galleries

--- gists/the-easiest-gallery-in-the-world/twentyfour_gallery.js
+++ gists/the-easiest-gallery-in-the-world/twentyfour_gallery.js
@@ -1,30 +1,30 @@
 /**
- * jQuery.browser.mobile (http://detectmobilebrowser.com/)
- *
- * jQuery.browser.mobile will be true if the browser is a mobile device
- *
- **/
+* jQuery.browser.mobile (http://detectmobilebrowser.com/)
+*
+* jQuery.browser.mobile will be true if the browser is a mobile device
+*
+**/
 (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
 //extend jquery
 (function($) {
-          // duck-punching to make attr() return a map
-          var _old = $.fn.attr;
-          $.fn.attr = function() {
-            var a, aLength, attributes, map;
-            if (this[0] && arguments.length === 0) {
-                    map = {};
-                    attributes = this[0].attributes;
-                    aLength = attributes.length;
-                    for (a = 0; a < aLength; a++) {
-                            map[attributes[a].name.toLowerCase()] = attributes[a].value;
-                    }
-                    return map;
-            } else {
-                    return _old.apply(this, arguments);
-            }
-    }
-  }(jQuery));
+        // duck-punching to make attr() return a map
+        var _old = $.fn.attr;
+        $.fn.attr = function() {
+          var a, aLength, attributes, map;
+          if (this[0] && arguments.length === 0) {
+                  map = {};
+                  attributes = this[0].attributes;
+                  aLength = attributes.length;
+                  for (a = 0; a < aLength; a++) {
+                          map[attributes[a].name.toLowerCase()] = attributes[a].value;
+                  }
+                  return map;
+          } else {
+                  return _old.apply(this, arguments);
+          }
+  }
+}(jQuery));
 //end extend jquery
 
 var nexus = {
@@ -85,6 +85,7 @@ function twentyfour_gallery (selector, settings){
     minimum_slides:2,
     fullsize_image_attribute:'data-imageurl',
     unveil_delay:2000,
+    enable_single_image_lightbox:true,
     templates:{
       gallery:'<div id="(#id#)" (#attributes#)>(#stage#)(#tray#)(#media_bar#)</div>',
       stage:'<div class="stage"><div class="fullscreen toggler icon (#fullscreen_icon_class#)"></div><div class="action_area nav left"> <div class="icon left (#nav_left_icon_class#)"></div></div><div class="slider">(#slides#)</div><div class="action_area nav right"><div class="icon right (#nav_right_icon_class#)"></div></div></div>',
@@ -378,6 +379,10 @@ function twentyfour_gallery (selector, settings){
       }
       gallery = this;
       this.container = jElement;
+      this.attributes = this.container.attr();
+      this.update_settings(settings);
+      this.update_settings(this.container.data());
+      this.settings.id = this.container.attr("id") || this.settings.prefix + Math.round(Math.random()*100);
 
       var slides = $(this.container).find(".slide");
       if(slides.length < this.settings.minimum_slides){
@@ -385,6 +390,10 @@ function twentyfour_gallery (selector, settings){
         // console.warn("too few slides");
         // console.info(this.container);
         // console.groupEnd();
+        if(gallery.settings.enable_single_image_lightbox){
+          var img_src = jElement.find("img").attr("src");
+          jElement.wrap("<a data-lightbox='"+this.settings.id+"' href='"+img_src+"' ></a>");
+        }
         return null;
       }
       else{
@@ -393,11 +402,6 @@ function twentyfour_gallery (selector, settings){
       $(slides).each(function(index){
         gallery.slides[index] = $(this);
       });
-
-      this.attributes = this.container.attr();
-      this.update_settings(settings);
-      this.update_settings(this.container.data());
-      this.settings.id = this.container.attr("id") || this.settings.prefix + Math.round(Math.random()*100);
 
       this.container.replaceWith(this.generate_gallery());
       this.container  = $("#" + this.settings.id);
@@ -423,6 +427,11 @@ function twentyfour_gallery (selector, settings){
 
 $(document).ready(function(){
   $("[data-embed24=Images]").each(function(){
-      new twentyfour_gallery($(this));
+    new twentyfour_gallery($(this));
   });
+  if (lightbox) {
+        if (lightbox.init) {
+            lightbox.init();
+        }
+    }
 });
