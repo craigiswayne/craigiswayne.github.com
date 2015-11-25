1c3dc1341361abe576ebc7222ae7de2f57e0d229
Craig Wayne <craig.wayne01@gmail.com>
2015-11-25 18:49:33 +0200

    WIP | Newsletters | Focus Block

--- gists/newsletters/engine/_newsletter_options.php
+++ gists/newsletters/engine/_newsletter_options.php
@@ -1,4 +1,13 @@
 <?php
   $title = "Netwerk24 Newsletter";
   $full_newsletter_width = "700px";
+
+  $color_accent = "#F5F5F5";
+  $color_border = "#d7d7d7";
+  $color_primary = "#2d3092";
+  $color_border_light = "#d7d7d7";
+
+  $gap = "10px";
+  $gap_small = "5px";
+
 ?>
