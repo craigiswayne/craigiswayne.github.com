a97a2788f04ca688c424d519daf1516e7b4c58ab
Craig Wayne <craigiswayne@gmail.com>
2025-09-29 15:35:46 +0100

    Cypress shits

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const {defineConfig} = require('cypress')
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: process.env.CYPRESS_BASE_URL || 'https://craigiswayne.com',
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: false,
+        testIsolation: false,
+        viewportWidth: 1920,
+        viewportHeight: 1080
+    },
+    video: false,
+    screenshotOnRunFailure: true
+})
---
+++ cypress/e2e/base-checks.cy.js
@@ -0,0 +1,109 @@
+describe('Base Checks', () => {
+
+    before(() => {
+        cy.visit('/');
+    })
+
+    it('Head tag checks', () => {
+        cy.title().should('eq', 'Craig Wayne Portfolio');
+        cy.get('head link[rel="shortcut icon"]').should('be.exist');
+
+    })
+
+    it('Header', () => {
+        cy.get('nav .container .flex > button.text-lg')
+            .should('be.visible')
+            .should('have.text', 'Craig Wayne');
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:first-child')
+            .should('be.visible')
+            .should('have.text', 'About');
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(2)')
+            .should('be.visible')
+            .should('have.text', 'Skills');
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(3)')
+            .should('be.visible')
+            .should('have.text', 'Projects');
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(4)')
+            .should('be.visible')
+            .should('have.text', 'Experience');
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(5)')
+            .should('be.visible')
+            .should('have.text', 'Contact');
+
+        cy.get('nav a[download]')
+            .should('be.visible')
+            .should('contain.text', 'Download CV')
+            .should('have.attr', 'download', 'CV-CraigWayne.pdf');
+
+    })
+
+    it('Header scroll navigation', () => {
+        cy.get('nav .container .flex .hidden.md\\:flex button:first-child')
+            .click()
+        cy.window().its('scrollY').should('be.greaterThan', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex > button.text-lg')
+            .click();
+
+        cy.window().its('scrollY').should('eq', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(2)')
+            .click()
+        cy.window().its('scrollY').should('be.greaterThan', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex > button.text-lg')
+            .click();
+
+        cy.window().its('scrollY').should('eq', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(3)')
+            .click()
+        cy.window().its('scrollY').should('be.greaterThan', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex > button.text-lg')
+            .click();
+
+        cy.window().its('scrollY').should('eq', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(4)')
+            .click()
+        cy.window().its('scrollY').should('be.greaterThan', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex > button.text-lg')
+            .click();
+
+        cy.window().its('scrollY').should('eq', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex .hidden.md\\:flex button:nth-child(5)')
+            .click()
+        cy.window().its('scrollY').should('be.greaterThan', 0);
+
+        cy.wait(500);
+
+        cy.get('nav .container .flex > button.text-lg')
+            .click();
+
+        cy.window().its('scrollY').should('eq', 0);
+    })
+})
